1e3b53fdf57c3ff0d8fadce5303e434a40b793a3
Spencer Bergamo <spencerbergamo@gmail.com>
2025-07-19 19:42:19 -0400

    expo imagepicker issue

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { Button, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function App() {
+
+  async function openImagePicker() {
+
+    // Make sure to select more than one video file to 
+    // get the delayed response 
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsMultipleSelection: true,
+      mediaTypes: ['images', 'videos'],
+    });
+
+    // either check the result wasn't canceled or if the result has assets
+    if (!result.canceled) {
+      console.log(result.assets.length);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Open Image Picker" onPress={openImagePicker} />
+    </View>
+  );
+}
